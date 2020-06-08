90dde858773f1ec9834b314967bc71691dfe883b
Frank Dornberger <frank@dornberger.ch>
2020-06-08 16:58:55 +0200

    Add base config and explanation for the NR Terraform provider

---
+++ main.tf
@@ -0,0 +1,20 @@
+# For further details on how to write Dashboards using Terraform, please visit:
+# https://www.terraform.io/docs/providers/newrelic/r/dashboard.html
+
+
+# The default environment variable is NEWRELIC_API_KEY and picked up if no 
+# variable is set in the provider configuration. To be able to explicitly
+# manage the variable and work with the below example, you can run this in 
+# your CLI: export TF_VAR_my_admin_api_key=NRAA-0123456789abcdef
+#
+# The API URL is required, as it would otherwise default to the US region.
+# Other values that can be set are found here: 
+# https://www.terraform.io/docs/providers/newrelic/index.html#argument-reference
+provider "newrelic" {
+  api_key = var.my_admin_api_key
+  api_url = "https://api.eu.newrelic.com/v2"
+}
+
+# data "newrelic_application" "my_application" {
+#   name = "My Application"
+# }
