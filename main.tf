45a79e9b219fb315650e73b5ba2c44ff08268f46
Frank Dornberger <frank@dornberger.ch>
2020-06-09 12:31:56 +0200

    Add a requirements block

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@
 # manage the variable and work with the below example, you can run this in 
 # your CLI: export TF_VAR_my_admin_api_key=NRAA-0123456789abcdef
 #
+
+terraform {
+  required_providers {
+    newrelic = ">= 1.19.0"
+  }
+  required_version = ">= 0.12.0"
+}
 # The API URL is required, as it would otherwise default to the US region.
 # Other values that can be set are found here: 
 # https://www.terraform.io/docs/providers/newrelic/index.html#argument-reference
