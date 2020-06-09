b4afdd62c27053e2109d68a2e26c59e274db54df
Frank Dornberger <frank@dornberger.ch>
2020-06-09 19:00:24 +0200

    Increase readability of variables file

--- apm-dashboard/variables.tf
+++ apm-dashboard/variables.tf
@@ -1,11 +1,11 @@
 variable "app_duration_sla" {
   description = "The SLA in ms of your Application response time. Default 500"
-  default = 500
+  default     = 500
 }
 
 variable "app_error_sla" {
   description = "The SLA in percents of your Application error rate. Default is 0.5"
-  default = 0.5
+  default     = 0.5
 }
 
 variable "dashboard_id" {
@@ -14,7 +14,7 @@ variable "dashboard_id" {
 
 variable "db_duration_sla" {
   description = "The SLA in ms of your DB response time. Default 100"
-  default = 100
+  default     = 100
 }
 
 variable "logo_url" {
@@ -27,15 +27,15 @@ variable "my_admin_api_key" {
 
 variable "new_relic_rest_api_url" {
   description = "The REST API Endpoint for your account. It depends on the regiof of your account. Options are https://api.newrelic.com/v2 or https://api.eu.newrelic.com/v2. Default is https://api.newrelic.com/v2"
-  default =  "https://api.newrelic.com/v2"
+  default     =  "https://api.newrelic.com/v2"
 }
 
 variable "non_http_20x_rate" {
   description = "The SLA in percents of your HTTP responses being non 20x. Default is 5"
-  default = 5
+  default     = 5
 }
 
 variable "third_party_duration_sla" {
   description = "The SLA in ms of your 3rd party response time. Default 150"
-  default = 150
+  default     = 150
 }
